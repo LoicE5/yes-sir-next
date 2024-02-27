bedc15e01de4e74999766a59ba0df37361be41a7
Loïc <59501884+LoicE5@users.noreply.github.com>
2024-02-27 13:35:40 +0100

    Installed speed insights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import '@/styles/style.css'
 import '@/styles/oxanium.css'
 import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
             <Component {...pageProps} />
             <Analytics />
+            <SpeedInsights />
         </>
     )
 
