eb3e3b00cb8901a838dfeb7f73715f545ba23515
Loïc <59501884+LoicE5@users.noreply.github.com>
2024-09-17 08:09:23 +0200

    Keep DB Awake cron job update

--- src/pages/api/cronjobs/keep_db_awake.ts
+++ src/pages/api/cronjobs/keep_db_awake.ts
@@ -6,9 +6,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (req.headers.authorization !== `Bearer ${process.env.CRON_SECRET}`)
         return res.status(401).json({ message: 'Unauthorized' })
 
-    const { error } = await supabaseApi.from('codes').select('id').limit(1)
+    const { error } = await supabaseApi.from('codes').select('*').limit(1)
     
-    const message = error ? `Keep DB awake : the cron job ran successfully, but the database returned an error` : `Keep DB awake : cron job successful`
+    if (error)
+        return res.status(500).json({message: `Keep DB awake : the cron job ran successfully, but the database returned an error`})
 
-    return res.json({ message: message })
+    return res.json({ message: `Keep DB awake : cron job successful` })
 }
